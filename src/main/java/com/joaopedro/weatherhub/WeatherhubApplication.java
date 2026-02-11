071fd195f50ee7ef868d46aae7daccc5a5003d54
João Pedro <joaopedrofvalle@gmail.com>
2026-02-11 11:49:04 -0300

    inicial: projeto Spring Boot

---
+++ src/main/java/com/joaopedro/weatherhub/WeatherhubApplication.java
@@ -0,0 +1,13 @@
+package com.joaopedro.weatherhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaopedro/weatherhub/WeatherhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaopedro.weatherhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
